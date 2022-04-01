7ee4f2b376adb2b7f91413c10d592b5597560c89
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-04-01 22:36:38 +0200

    add mui

--- src/components/Courses/Menu/Menu.jsx
+++ src/components/Courses/Menu/Menu.jsx
@@ -1,15 +1,62 @@
 import React from 'react';
 import { MenuBox } from './Menu.styled';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+
+const style = {
+  width: '100%',
+  maxWidth: 260,
+  fontSize: '14',
+  bgcolor: 'rgba(55, 71, 79, 1)',
+  color: 'rgba(255, 255, 255, 0.72)',
+};
 
 const Menu = () => {
   return (
     <>
-      <MenuBox>
-        <p>Menu</p>
-        <p>Menu</p>
-        <p>Menu</p>
-        <p>Menu</p>
-      </MenuBox>
+      <Box sx={style}>
+        <Typography variant="h4" component="div">
+          logo
+        </Typography>
+        <Divider />
+        <List sx={style} component="nav" aria-label="mailbox folders">
+          <ListItem button>
+            <DashboardOutlinedIcon sx={{ fontSize: 24 }} />
+            <ListItemText primary="Dashboad" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Kursy" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Produkty" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Użytkownicy" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Marketing" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Sprzedaż" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Repozytorium" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Wyloguj" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Nazwa firmy" />
+          </ListItem>
+        </List>
+      </Box>
     </>
   );
 };
--- src/components/Courses/Menu/Menu.styled.jsx
+++ src/components/Courses/Menu/Menu.styled.jsx
@@ -3,5 +3,7 @@ import styled from '@emotion/styled';
 export const MenuBox = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   background-color: rgba(55, 71, 79, 1);
+  width: 260px;
 `;
