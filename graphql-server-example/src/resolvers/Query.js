412cf9bf9b53128e1e053379225c01ecf429f081
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-05-30 13:22:02 +0200

    add graphql, schema.graphql, query

---
+++ graphql-server-example/src/resolvers/Query.js
@@ -0,0 +1,13 @@
+function students(parent, args) {
+  return {
+    id: 1,
+    name: 'Misha',
+    address: [{ city: 'Gorzow' }],
+    email: 'email.com',
+    phone: '777 777 777',
+  };
+}
+
+module.exports = {
+  students,
+};
--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <BrowserRouter>
@@ -18,7 +34,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
