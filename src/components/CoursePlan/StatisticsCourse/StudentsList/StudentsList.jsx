2ed00c2516da7e66a4785730a88f026adb2f4669
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-06-02 09:40:29 +0200

    fetch_students

--- src/components/CoursePlan/StatisticsCourse/StudentsList/StudentsList.jsx
+++ src/components/CoursePlan/StatisticsCourse/StudentsList/StudentsList.jsx
@@ -19,18 +19,18 @@ const Students_QUERY = gql`
 `;
 
 const StudentsList = () => {
-  // const [students, setStudents] = useState([]);
-  // useEffect(() => {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //     .then(response => response.json())
-  //     .then(json => setStudents(json));
-  // }, []);
+  const [students, setStudents] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => setStudents(json));
+  }, []);
 
   const { loading, error, data } = useQuery(Students_QUERY);
 
   return (
     <>
-      <List>
+      {/* <List>
         <Grid container spacing={2}>
           {loading === true ? (
             <div>L o a d i n g . . .</div>
@@ -39,8 +39,27 @@ const StudentsList = () => {
               <StudentCard
                 key={student.id}
                 name={student.name}
-                city={student.address}
-                // city={student.address.city} --- only fetch
+                city={student.address} //---QUERY
+                city={student.address.city} //--- only fetch
+                email={student.email}
+                phone={student.phone}
+                bgcolor={'rgba(0, 172, 193, 1)'}
+              />
+            ))
+          )}
+        </Grid>
+      </List> */}
+      <List>
+        <Grid container spacing={2}>
+          {students.length === 0 ? (
+            <div>L o a d i n g . . .</div>
+          ) : (
+            students.map(student => (
+              <StudentCard
+                key={student.id}
+                name={student.name}
+                // city={student.address} //---QUERY
+                city={student.address.city} //--- only fetch
                 email={student.email}
                 phone={student.phone}
                 bgcolor={'rgba(0, 172, 193, 1)'}
